33a09f4fc1a2fe84c8ca815b616f0c30a1b023be
Abinasha Karana <abinash@bizosys.com>
2013-05-03 15:20:23 +0530

    Naming convention changes,

--- src/java/com/bizosys/hsearch/functions/HSearchReducer.java
+++ src/java/com/bizosys/hsearch/functions/HSearchReducer.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.util.Collection;
 
 public interface HSearchReducer {
-	void appendCols(StatementWithOutput[] queryOutput, Collection<byte[]> mergedQueryOutput) throws IOException;
-	void appendRows(Collection<byte[]> mergedRows, Collection<byte[]> appendRows) throws IOException;
-	void appendRows(Collection<byte[]> mergedRows, byte[] appendRowId, Collection<byte[]> appendRows) throws IOException;
+	void appendCols(StatementWithOutput[] queryOutput, Collection<byte[]> finalColumns) throws IOException;
+	void appendRows(Collection<byte[]> finalOutput, Collection<byte[]> partOutput) throws IOException;
+	void appendRows(Collection<byte[]> finalOutput, byte[] appendRowId, Collection<byte[]> partOutput) throws IOException;
 }
